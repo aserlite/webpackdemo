ed9405102f171357e0eeb28f9562366a7153d690
Arthur Cuvillon <arthurcuvillon691@gmail.com>
2023-11-10 11:34:33 +0100

    add tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-console.log('test');
+
 module.exports = {
     watchOptions: {
         ignored: /node_modules/
@@ -14,15 +14,23 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "index.html",
-            clean:true,
-
-        })
+            clean: true,
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    'postcss-loader',
+                ],
             }
         ],
     },
